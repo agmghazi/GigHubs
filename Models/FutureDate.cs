90ff572f5c5b9447950af503c81083525e1ff2c9
Ahmed Gamal <gis.csharp@gmail.com>
2019-05-25 11:35:20 +0300

    Implement Validation

---
+++ Models/FutureDate.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Globalization;
+
+namespace GigHub.Models
+{
+    public class FutureDate : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            var tryDate = DateTime.TryParseExact(Convert.ToString(value),
+                "d MMM yyyy",
+                CultureInfo.CurrentCulture,
+                DateTimeStyles.None,
+                out var dateTime);
+            return (tryDate && dateTime > DateTime.Now);
+        }
+    }
+}
