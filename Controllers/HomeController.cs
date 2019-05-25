ef11bdcf5ae1e043994497bb81537d3629e0f329
Ahmed Gamal <gis.csharp@gmail.com>
2019-05-25 15:37:05 +0300

    Add name to artist.

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,16 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using GigHub.Models;
+using System;
+using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace GigHub.Controllers
 {
     public class HomeController : Controller
     {
+        private ApplicationDbContext _context;
+
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upComingGig = _context.Gigs
+                .Include(x => x.Artist)
+                .Where(x => x.DateTime > DateTime.Now);
+            return View(upComingGig);
         }
 
         public ActionResult About()
